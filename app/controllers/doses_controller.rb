249e802cf8675025577a0510172bf71db3676098
Benjamin Chen <yeehenchen@gmail.com>
2018-11-15 17:00:23 +0800

    Optional 1, added dose form in cocktail show page, doses/new.html.erb could be killed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,11 +8,8 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    if @dose.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
+    @dose.save
+    redirect_to cocktail_path(@cocktail), notice: @dose.errors.full_messages
   end
 
   def destroy
@@ -20,6 +17,8 @@ class DosesController < ApplicationController
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
   end
+
+  private
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,4 @@
 import 'bootstrap';
 
 console.log('Hello World from Webpacker')
+
