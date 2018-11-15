e9e9e663f2e8b2305dea9b6d4eb899ee7f3f6d3c
Sz-Ting, Chen <kimikamis@gmail.com>
2018-11-15 14:40:38 +0800

    Add dose interface & dose controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
 end
